fb4313ebb41714509daa3a17df3f8f53a1fbe10d
yuweid <yuweid@andrew.cmu.edu>
2025-01-23 11:34:40 -0500

    update Main to depend on the Shape interface

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,23 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape square = new Square(1.5);
+        Shape circle = new Circle(2);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer rectangleRenderer = new Renderer(rectangle);
+        rectangleRenderer.draw();
 
-        renderer.draw();
+        Renderer circleRenderer = new Renderer(circle);
+        circleRenderer.draw();
+
+        Renderer squareRenderer = new Renderer(square);
+        squareRenderer.draw();
     }
 }
