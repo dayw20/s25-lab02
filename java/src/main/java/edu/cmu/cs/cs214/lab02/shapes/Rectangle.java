0d65c2d2865e9249aae2e81fa36a2c1ad3c41234
yuweid <yuweid@andrew.cmu.edu>
2025-01-23 12:27:38 -0500

    implement setter and getter, also remove the rectangle interface

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,14 +1,35 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
+    private double height;
+    private double width;
     
     public Rectangle(double height, double width){
+        if(height <= 0) throw new IllegalArgumentException("Invalid Height: height must be positive");
+        if(width <= 0) throw new IllegalArgumentException("Invalid Width: width must be positive");
         this.height = height;
         this.width = width;
     }
 
+
+    //setter and getter
+    public double getHeight(){
+        return height;
+    }
+
+    public void setHeight(double h){
+        this.height = h;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public void setWidth(double w){
+        this.width = w;
+    }
+
+    @Override
     public double getArea() {
         return height * width;
     }
