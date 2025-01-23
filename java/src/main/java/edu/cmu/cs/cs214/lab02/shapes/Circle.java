7761cb0d31ba59391361af298c5cf127ec58c9ba
达雨薇 <yvette@Mac.lan>
2025-01-23 01:41:08 -0500

    modify Circle.java: make radius private and constant

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
@@ -1,12 +1,14 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Circle implements Shape {
-    public double radius;
+    private final double radius;
 
     public Circle(double radius) {
+        if (radius <= 0) throw new IllegalArgumentException("Invalid Radius: Radius must be positive");
         this.radius = radius;
     }
 
+    @Override
     public double getArea() {
         return Math.PI * radius * radius;
     }
