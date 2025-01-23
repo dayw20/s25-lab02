5954017bdc6b78f77f31fe2a6d608cccebad4747
yuweid <yuweid@andrew.cmu.edu>
2025-01-23 11:18:49 -0500

    also modify the rectangle and square

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
@@ -1,12 +1,23 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Square implements Shape {
-    public double sideLen;
+    private double sideLen;
 
     public Square(double sideLen) {
+        if(sideLen <= 0) throw new IllegalArgumentException("Invalid Sidelen: sidelen must be positive");
         this.sideLen = sideLen;
     }
     
+    //setter and getter
+    public double getSidelen(){
+        return sideLen;
+    }
+
+    public void setSidelen(sl){
+        this.sideLen = sl;
+    }
+    
+    @Override
     public double getArea() {
         return sideLen * sideLen;
     }
