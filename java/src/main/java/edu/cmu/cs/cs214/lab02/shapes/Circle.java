29efa3be0426624f84ac5b146ed768e9d0c9a731
yuweid <yuweid@andrew.cmu.edu>
2025-01-23 11:11:03 -0500

    implement setter and getter in Circle

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
@@ -1,11 +1,19 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Circle implements Shape {
-    private final double radius;
+    private double radius;
 
     public Circle(double radius) {
         if (radius <= 0) throw new IllegalArgumentException("Invalid Radius: Radius must be positive");
         this.radius = radius;
+    }
+    //setter and getter
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double r){
+        this.radius = r;
     }
 
     @Override
